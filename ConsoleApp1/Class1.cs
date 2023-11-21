e4f238966e9a58bbed8d18ebf6632e9edd363afb
Moon <Moon@DESKTOP-162JJ4K>
2023-11-21 21:23:45 +0200

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace klase1
+{
+    public class seka
+    {
+        //teko pasinaudoti void, nes while skliaustus priskyre prie public class seka ir dave daug erroru.
+        public void sek()
+            { 
+            double a = 1;
+            double b = 2;
+            double c = 1;
+            double i = 0;
+            double l = Math.Pow(10, 15);
+
+            while (c < l)
+            {
+                c = a * b;
+                a = b;
+                b = c;
+                i = c;
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace Consoleapp1
+{
+   class programa
+    {
+        static void Main(string[] args)
+        {
+            //skaiciavimui kiek programa veiks laiko
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+
+            //naudojama klasė iš kitos bylos
+            klase1.seka myObj = new klase1.seka();
+            myObj.sek();
+
+            //baigiama skaiciuoti programos veikimo laika.
+            stopwatch.Stop();
+
+            Console.WriteLine("Praejas laikas: {0} ms", stopwatch.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
